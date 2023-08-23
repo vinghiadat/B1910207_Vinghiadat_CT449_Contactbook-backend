b6a44c91cd56ccbc75a77f31a8779035dec3f8dd
ViNghiaDat <Datb1910207@student.ctu.edu.vn>
2023-08-23 10:02:12 +0700

    Cài đặt xử lý lỗi

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const cors = require("cors");
 
 const app = express();
-const contactsRouter = require("./app/routes/contact.route");
-app.use(cors());
-app.use(express.json());
-app.use("/api/contacts", contactsRouter);
 
-app.get("/", (reg, res) => {
-    res.json({message: "Welcome to contact book app;ication"});
+app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
 });
 
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+app.use(cors());
+app.use(express.json());
+app.get("/", (reg, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
 module.exports = app;
