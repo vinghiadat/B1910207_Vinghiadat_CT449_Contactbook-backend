fbaaa6d8d047592fe45177b3c334dfe978ace1f3
ViNghiaDat <Datb1910207@student.ctu.edu.vn>
2023-08-23 09:47:20 +0700

    Cài đặt các handler truy xuất CSDL

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (reg, res) => {
     res.json({message: "Welcome to contact book app;ication"});
